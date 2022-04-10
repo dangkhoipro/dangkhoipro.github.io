4b37f90fabf0323cb7681be5d7ec4b1e941d6d66
khoind4 <khoind4@vng.com.vn>
2022-04-10 16:33:20 +0700

    Init send mail via smtpjs

--- public/index.js
+++ public/index.js
@@ -12,4 +12,35 @@ function handleSideBar() {
     });
 }
 
+function handleForm() {
+    const form = document.querySelector('#form');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const inputs = Array.from(document.querySelectorAll('#form input, #form textarea'));
+        const data = inputs.reduce((acc, input) => ({
+            ...acc,
+            [input.id]: input.value
+        }), {});
+        sendEmail(data);
+    });
+}
+
+function sendEmail(data) {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "dangkhoi.tinhoc1@gmail.com",
+        Password: "9E0D93DFA276091DCD5AEFA027BC7A78A553",
+        To: 'dangkhoi.tinhoc1@gmail.com',
+        From: data.email,
+        Subject: `${data.name || 'Someone'} contacted me via github page`,
+        Body: data.content
+    }).then(message => {
+        alert(message);
+        const form = document.querySelector('#form');
+        form.reset();
+    });
+}
+
 handleSideBar();
+handleForm();
